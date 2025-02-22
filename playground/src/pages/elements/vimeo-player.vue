e5f9d0b30d889d41b05b296d7fc34208c1658b88
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-02-22 09:31:07 +0200

    feat: Enhance VimeoPlayer with thumbnail and event handling

--- playground/src/pages/elements/vimeo-player.vue
+++ playground/src/pages/elements/vimeo-player.vue
@@ -1,10 +1,49 @@
 <template>
-  <Box class="pageElementsVimeoPlayer" w="640" h="320">
-    <VimeoPlayer v-bind="{ videoId: 935361750, background: true }" />
+  <Box class="pageElementsVimeoPlayer" w="100vw" h="100vh">
+    <VimeoPlayer v-bind="{ videoId: 1058475905, background: true }" thumbnail-url="/assets/imgs/genzu.jpg"
+      @play="onPlay" @pause="onPause" @ended="onEnded" @error="onError" @loaded="onLoaded" @bufferend="onBufferEnd"
+      @bufferstart="onBufferStart" @playbackratechange="onPlaybackRateChange" @progress="onProgress" @seeked="onSeeked"
+      @timeupdate="onTimeUpdate" @volumechange="onVolumeChange" />
   </Box>
 </template>
 
-<script setup>
+<script setup lang="ts">
+const onPlay = () => {
+  console.log('play')
+}
+const onPause = () => {
+  console.log('pause')
+}
+const onEnded = () => {
+  console.log('ended')
+}
+const onError = () => {
+  console.log('error')
+}
+const onLoaded = () => {
+  console.log('loaded')
+}
+const onBufferEnd = () => {
+  console.log('bufferend')
+}
+const onBufferStart = () => {
+  console.log('bufferstart')
+}
+const onPlaybackRateChange = () => {
+  console.log('playbackratechange')
+}
+const onProgress = () => {
+  console.log('progress')
+}
+const onSeeked = () => {
+  console.log('seeked')
+}
+const onTimeUpdate = () => {
+  console.log('timeupdate')
+}
+const onVolumeChange = () => {
+  console.log('volumechange')
+}
 </script>
 
 <style lang="scss"></style>
