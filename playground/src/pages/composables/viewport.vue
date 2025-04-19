e05e33f8a992282e88ee0921771cd2a529f22fcf
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-04-19 13:54:20 +0900

    feat: Viewport コンポーネントを追加し、サイズ管理機能を実装

---
+++ playground/src/pages/composables/viewport.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<Typography center color="100">
+			width ? {{ width }}<br>
+			height ? {{ height }}<br>
+			initWidth ? {{ initWidth }}<br>
+			initHeight ? {{ initHeight }}<br>
+			keyboardHeight ? {{ keyboardHeight }}
+		</Typography>
+	</div>
+</template>
+
+<script setup>
+const viewport = useViewport()
+const { width, height, initWidth, initHeight, keyboardHeight } = viewport
+</script>
+
+<style lang="scss"></style>
