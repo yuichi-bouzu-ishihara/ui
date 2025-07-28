26b5c50d2581c37bc0b82c6df93c77c366d977f2
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-07-28 17:00:07 +0900

    feat: アイコンリストを更新し、新しいSVGアイコンを追加。不要なスクリプトとスタイルを削除し、アイコンの参照方法を改善。

--- src/runtime/composables/elements/icon.ts
+++ src/runtime/composables/elements/icon.ts
@@ -3,14 +3,7 @@
  */
 import type { UIConfig } from '../../types'
 import type { IconConfig } from '../../types/icon'
-import List from '../../assets/bouzu-ui/icons/list.json'
 import { useAppConfig, useState, readonly } from '#imports'
-
-// Types -----------------------------------------
-type IconReference = {
-	class: string
-	path: string
-}
 
 export const useIcon = () => {
 	const config = useState<IconConfig | null>('ui-icon-config', () => null)
@@ -26,19 +19,9 @@ export const useIcon = () => {
 	/**
 	 * Icon 参照データを返す
 	 */
-	const reference = (name: string): IconReference => {
-		const data: IconReference = { class: '', path: '' }
-		if (!config.value) return data
-
-		if (List.includes(name)) {
-			data.class = `_${name}`
-		}
-		else {
-			if (config.value.dir) {
-				data.path = `${config.value.dir}/${name}.svg`
-			}
-		}
-		return data
+	const reference = (name: string): string => {
+		if (!config.value) return ''
+		return `${config.value.dir}/${name}.svg`
 	}
 
 	return {
