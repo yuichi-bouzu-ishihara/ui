c512cb76359d134bfc8e3aa372df95d8f0cc7f99
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-08-25 11:56:45 +0300

    feat: useDebounce から maxWait を省いて機能シンプル化

---
+++ playground/src/pages/composables/Debounce.vue
@@ -0,0 +1,27 @@
+<template>
+	<Container narrow>
+		<Column gap="20">
+			<Typography center color="light">
+				Click Count: {{ clickCount }} / <strong>Debounce Count: {{ debounceCount }}</strong>
+			</Typography>
+			<Button @click="onClick">
+				Debounce
+			</Button>
+		</Column>
+	</Container>
+</template>
+
+<script setup>
+const clickCount = ref(0)
+const debounceCount = ref(0)
+const test = () => {
+	debounceCount.value++
+}
+const debounce = useDebounce(test, 1000)
+const onClick = () => {
+	clickCount.value++
+	debounce()
+}
+</script>
+
+<style lang="scss"></style>
--- src/runtime/composables/debounce.ts
+++ src/runtime/composables/debounce.ts
@@ -2,5 +2,5 @@ import { debounce } from 'ts-debounce'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useDebounce = <T extends (...args: any[]) => any>(fn: T, ms: number) => {
-	return debounce(fn, ms, { maxWait: ms })
+	return debounce(fn, ms)
 }
