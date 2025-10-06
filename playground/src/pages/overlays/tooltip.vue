628dd30c3cd14c784ee8cb1bddd3d8b7dfd57566
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-10-06 07:56:11 +0300

    feat: tooltip.vueのスタイルを更新し、背景画像を追加。アイコンボタンのスタイルを変更し、全体のレイアウトを調整。

--- playground/src/pages/overlays/tooltip.vue
+++ playground/src/pages/overlays/tooltip.vue
@@ -1,29 +1,27 @@
 <template>
-	<Center class="pageTooltip">
-		<Box w="100" relative>
-			<Column gap="56">
-				<Column justify="center" fit>
-					<Tooltip text="Tooltip Up">
-						<IconButton :icon="{ name: 'arrowUp' }" tertiary xsmall rounded @click="handleClick('up')" />
-					</Tooltip>
-					<Row justify="between" fit-w>
-						<Tooltip text="Tooltip Left" placement="left">
-							<IconButton :icon="{ name: 'arrowLeft' }" tertiary xsmall rounded @click="handleClick('left')" />
+	<Center class="pageOverlayTooltip"
+		style="background-image: url(https://cdn.pixabay.com/photo/2024/05/26/15/27/anime-8788959_1280.jpg);">
+		<Box w="100vw" pt="80" pb="80" relative z-index="0">
+			<Box w="100" m="auto" relative>
+				<Column gap="56">
+					<Column justify="center" fit>
+						<Tooltip text="Tooltip Up">
+							<IconButton :icon="{ name: 'arrowUp' }" secondary xsmall rounded @click="handleClick('up')" />
 						</Tooltip>
-						<Tooltip text="Tooltip Right" placement="right">
-							<IconButton :icon="{ name: 'arrowRight' }" tertiary xsmall rounded @click="handleClick('right')" />
+						<Row justify="between" fit-w>
+							<Tooltip text="Tooltip Left" placement="left">
+								<IconButton :icon="{ name: 'arrowLeft' }" secondary xsmall rounded @click="handleClick('left')" />
+							</Tooltip>
+							<Tooltip text="Tooltip Right" placement="right">
+								<IconButton :icon="{ name: 'arrowRight' }" secondary xsmall rounded @click="handleClick('right')" />
+							</Tooltip>
+						</Row>
+						<Tooltip text="Tooltip Down" placement="down">
+							<IconButton :icon="{ name: 'arrowDown' }" secondary xsmall rounded @click="handleClick('down')" />
 						</Tooltip>
-					</Row>
-					<Tooltip text="Tooltip Down" placement="down">
-						<IconButton :icon="{ name: 'arrowDown' }" tertiary xsmall rounded @click="handleClick('down')" />
-					</Tooltip>
+					</Column>
 				</Column>
-				<Tooltip text="Default Open" placement="left" default-open>
-					<Typography caption3 center nowrap cap-height-baseline>
-						👈
-					</Typography>
-				</Tooltip>
-			</Column>
+			</Box>
 		</Box>
 	</Center>
 </template>
