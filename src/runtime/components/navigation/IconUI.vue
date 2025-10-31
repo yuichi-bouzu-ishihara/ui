a83b2cc427c3f92d35a9773811bf7081c697d039
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-10-31 16:05:07 +0900

    fix: import 'ref' in IconUI component for improved reactivity

--- src/runtime/components/navigation/IconUI.vue
+++ src/runtime/components/navigation/IconUI.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { useBreakPoint } from '../../composables/break-point'
 import BasicLink from '../elements/BasicLink.vue'
