f73901963f4db0945c119d9579bd60919f602410
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-11-23 10:59:45 +0900

    feat(vimeo-player-multiple): enhance error handling and improve key management
    - Added event handlers for buffer and loop errors to log issues and track reset counts.
    - Updated key management for video components to ensure unique keys based on video ID and reset count.
    - Included documentation for video IDs and development setup in the script section.

--- playground/src/pages/test/vimeo-player-multiple.vue
+++ playground/src/pages/test/vimeo-player-multiple.vue
@@ -1,15 +1,37 @@
 <template>
 	<div class="pageTestVimeoPlayerMultiple">
 		<Row split="4">
-			<Ratio v-for="item in list" :key="item.videoId" wide-screen>
-				<VimeoPlayer v-model:muted="muted" v-model:volume="volume" :video-id="item.videoId" autoplay background loop
-					:autopause="false" :debug="item.debug" />
+			<Ratio v-for="(item, index) in list" :key="`${item.videoId}-${index}`" wide-screen>
+				<VimeoPlayer :key="`${item.videoId}-${index}-${resetCount[index]}`" v-model:muted="muted"
+					v-model:volume="volume" :video-id="item.videoId" autoplay background loop :autopause="false"
+					:debug="item.debug" @buffererror="onBufferError(index)" @looperror="onLoopError(index)" />
 			</Ratio>
 		</Row>
 	</div>
 </template>
 
 <script setup lang="ts">
+/**
+ * 以下は honoo.tokyo で再生可能な動画の ID です。
+ *
+ * @note 再生して確認する場合は、nuxt.config.ts の vite 設定に下記を追記してください。
+ * ```ts
+ * export default defineNuxtConfig({
+ *   vite: {
+ *     server: {
+ *       ...(process.env.NODE_ENV === 'development' ? {
+ *         host: 'local.honoo.tokyo',
+ *         port: 3000,
+ *       } : {}),
+ *     },
+ *   },
+ * })
+ * ```
+ * @note また、package.json の dev script に下記を追記してください。
+ * ```json
+ * "dev": "--host local.honoo.tokyo --port 3000",
+ * ```
+ */
 const list = ref([
 	{ videoId: '1134844894', debug: ['bufferstart', 'bufferend', 'progress'] },
 	{ videoId: '1129930408', debug: [] },
@@ -34,4 +56,18 @@ const list = ref([
 ])
 const muted = ref(true)
 const volume = ref(0.25)
+const resetCount = ref<number[]>(Array.from({ length: list.value.length }, () => 0))
+
+// Methods --------------------------------------------------
+const onBufferError = (index: number) => {
+	const videoId = list.value[index].videoId
+	console.log('Buffer error', videoId)
+	resetCount.value[index]++
+}
+
+const onLoopError = (index: number) => {
+	const videoId = list.value[index].videoId
+	console.log('Loop error', videoId)
+	resetCount.value[index]++
+}
 </script>
