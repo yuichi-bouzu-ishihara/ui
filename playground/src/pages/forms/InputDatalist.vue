66ff65178bc53f6cb650fdc1126c3092bed70aeb
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-11-24 10:47:39 +0900

    feat(InputDatalist): add new InputDatalist component with basic functionality
    - Created InputDatalist component for enhanced user input with a dropdown list.
    - Implemented template structure, reactive state management, and basic styling.
    - Added support for focus handling and dynamic list filtering based on user input.

---
+++ playground/src/pages/forms/InputDatalist.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="pageFormsInputDatalist">
+		<Column justify="center" gap="40">
+			<Typography title1 color="100">
+				<InputDatalist v-model="value" name="inputDatalistTest" label="Label" placeholder="Placeholder" :focus="focus"
+					:list="[
+						'item1',
+						'item2',
+						'item3',
+						'item4',
+						'item5',
+					]" />
+			</Typography>
+			<Switch v-model="focus" name="focus">
+				Focus
+			</Switch>
+		</Column>
+	</div>
+</template>
+
+<script setup>
+const value = ref('')
+const focus = ref(false)
+</script>
+
+<style lang="scss"></style>
