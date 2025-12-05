8d4049fb0273d3dee67d395210e35c9245627940
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-12-05 11:12:19 +0900

    refactor(FieldFooter): streamline message and description rendering
    - Updated FieldFooter component to simplify the rendering of message and description.
    - Added a new CSS class for text alignment in the footer.
    - Adjusted the template in field-footer.vue to include a right alignment for the Typography component.

--- playground/src/pages/forms/field-footer.vue
+++ playground/src/pages/forms/field-footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="pageFormsInput">
 		<Row justify="center" gap="8">
-			<Typography title1>
+			<Typography title1 right>
 				<Input v-model="value" name="inputTest" label="ラベル" placeholder="プレースホルダー" maxlength="10" validation="always"
 					focus @focus="focus = true" @blur="focus = false" @valid="fail = false" @invalid="fail = true" />
 				<FieldFooter v-bind="{ fail, focus, message }" description="This Area is Description Insert." />
