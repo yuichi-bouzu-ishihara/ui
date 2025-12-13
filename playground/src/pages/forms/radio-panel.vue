151b89b70c727a73fea8a24a70160d99ba82439f
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2025-12-14 08:06:46 +0900

    feat(RadioPanel): add free input option and enhance selection handling
    - Introduced a new RadioPanel option that allows for free input with configurable parameters.
    - Updated the selection logic to manage the state of the selected option and free input value.
    - Enhanced the template structure for better layout and user experience.

--- playground/src/pages/forms/radio-panel.vue
+++ playground/src/pages/forms/radio-panel.vue
@@ -3,6 +3,8 @@
 		<Container>
 			<Column gap="56">
 				<RadioPanel v-model="value" large name="radioPanelTest" :options="LIST" split="5" gap="8" title="Large" />
+				<RadioPanel v-model="value" large name="radioPanelTestFree" :options="LIST_INCLUDE_FEE" split="5" gap="8"
+					title="Large" />
 				<RadioPanel v-model="value" name="radioPanelTest" :options="LIST" split="5" gap="8" title="Medium" />
 				<RadioPanel v-model="value" small name="radioPanelTest" :options="LIST" split="5" gap="8" title="Small" />
 				<RadioPanel v-model="value" xsmall name="radioPanelTest" :options="LIST" split="5" gap="8" title="XSmall" />
@@ -19,6 +21,13 @@ const LIST = [
 	{ value: 3, label: '3' },
 	{ value: 4, label: '4' },
 	{ value: 5, label: '5' },
+]
+const LIST_INCLUDE_FEE = [
+	{ value: 1, label: '1' },
+	{ value: 2, label: '2' },
+	{ value: 3, label: '3' },
+	{ value: 4, label: '4' },
+	{ value: 5, label: 'Free', free: { min: 1, max: 100, step: 100, default: 5 } },
 ]
 </script>
 
