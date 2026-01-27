bd4de5617a000c5a37a4e4b3b5df4810f11b2521
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2026-01-27 15:29:06 +0900

    fix: RadioPanelコンポーネントのv-modelを更新し、選択されたオプションの初期値を正しく反映するように修正しました。また、LISTの分割数を動的に設定するように変更しました。

--- playground/src/pages/forms/radio-panel.vue
+++ playground/src/pages/forms/radio-panel.vue
@@ -3,16 +3,20 @@
 		<Container>
 			<Column gap="56">
 				<Column gap="8">
-					<RadioPanel v-model="value" large name="radioPanelTest" :options="LIST" split="5" gap="8" title="Size" />
-					<RadioPanel v-model="value" name="radioPanelTest" :options="LIST" split="5" gap="8" />
-					<RadioPanel v-model="value" small name="radioPanelTest" :options="LIST" split="5" gap="8" />
-					<RadioPanel v-model="value" xsmall name="radioPanelTest" :options="LIST" split="5" gap="8" />
+					<RadioPanel v-model="value1" large name="radioPanelTest" :options="LIST" :split="LIST.length.toString()"
+						gap="8" title="Size" />
+					<RadioPanel v-model="value1" name="radioPanelTest" :options="LIST" :split="LIST.length.toString()" gap="8" />
+					<RadioPanel v-model="value1" small name="radioPanelTest" :options="LIST" :split="LIST.length.toString()"
+						gap="8" />
+					<RadioPanel v-model="value1" xsmall name="radioPanelTest" :options="LIST" :split="LIST.length.toString()"
+						gap="8" />
 				</Column>
-				<RadioPanel v-model="value" name="radioPanelTestFree" :options="LIST_INCLUDE_FEE" split="5" gap="8"
-					@checked-free="console.log('checked-free')" @unchecked-free="console.log('unchecked-free')">
+				<RadioPanel v-model="value2" name="radioPanelTestFree" :options="LIST_INCLUDE_FEE"
+					:split="LIST_INCLUDE_FEE.length.toString()" gap="8" @checked-free="console.log('checked-free')"
+					@unchecked-free="console.log('unchecked-free')">
 					<template #before-free-input>
 						<Typography body center bold>
-							¥{{ joinComma(value) }}
+							¥{{ joinComma(value2) }}
 						</Typography>
 					</template>
 				</RadioPanel>
@@ -23,8 +27,10 @@
 
 <script setup>
 const { joinComma } = useNumber()
-const value = ref(2)
+const value1 = ref(0)
+const value2 = ref(2)
 const LIST = [
+	{ value: 0, label: '0' },
 	{ value: 1, label: '1' },
 	{ value: 2, label: '2' },
 	{ value: 3, label: '3' },
@@ -36,7 +42,7 @@ const LIST_INCLUDE_FEE = [
 	{ value: 500, label: '¥500' },
 	{ value: 1000, label: '¥1,000' },
 	{ value: 5000, label: '¥5,000' },
-	{ value: 0, label: 'その他', free: { min: 100, max: 10000, step: 100, default: 500 } },
+	{ value: Infinity, label: 'その他', free: { min: 100, max: 10000, step: 100, default: 500 } },
 ]
 </script>
 
