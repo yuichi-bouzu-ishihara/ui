6ad0b27006d1c04acb56e618df2c63bffd3b7f6a
yuichi bouzu ishihara <yuichi.ishihara@bouzu.design>
2026-06-18 09:18:08 +0900

    fix: aspect-ratio.vue が親コンポーネントからはみ出して表示される不具合を修正

--- playground/src/pages/layout/aspect-ratio.vue
+++ playground/src/pages/layout/aspect-ratio.vue
@@ -1,18 +1,16 @@
 <template>
-	<Box w="100vw" pt="40">
-		<Center>
-			<Box w="400">
-				<AspectRatio :value="ratio">
-					<Box color="text-060" w="100%" h="100%" pt="4" pl="8" />
-				</AspectRatio>
-			</Box>
-		</Center>
+	<Box w="100vw" h="100%" pt="40">
+		<Box w="500" h="200">
+			<AspectRatio :value="ratio">
+				<Box color="text-060" w="100%" h="100%" pt="4" pl="8" />
+			</AspectRatio>
+		</Box>
 	</Box>
 </template>
 
 <script setup>
 // Data ------------------
-const ratio = ref('16 / 9')
+const ratio = ref('9 / 16')
 </script>
 
 <style lang="scss"></style>
